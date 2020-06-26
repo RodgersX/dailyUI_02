98d34ae2efd7734161772cdc224f8a84fd711d1b
RodgersX <brianmawira2@gmail.com>
2020-06-26 19:41:14 +0300

    responsiveness handled

---
+++ src/components/navigation.vue
@@ -0,0 +1,100 @@
+<template>
+    <div class="navigation">
+        <div class="imageDiv">
+        <img height="500" width="100%" :src="require('@/assets/main_image.png')">
+        <div class="toolbar d-flex justify-space-between align-center px-4">
+            <img class="logo" to="/" width="80" :src="require('@/assets/logo.png')">
+            <div class="navigationLinks hidden-sm-and-down">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Shop</a></li>
+                <li><a href="#">Contact</a></li>
+                <li>
+                <v-btn icon color="black"><i class="bx bx-search-alt-2 "></i></v-btn>
+                </li>
+                <li>
+                <v-btn icon color="black"><i class="bx bxs-lock-alt"></i></v-btn>
+                </li>
+            </ul>
+            </div>
+
+            <!-- mobile nav -->
+            <!-- hamburger menu -->
+            <v-app-bar-nav-icon
+            large
+            class="grey--text hidden-md-and-up nav"
+            @click="drawer =!drawer"></v-app-bar-nav-icon>
+
+            <v-navigation-drawer color="#333" dark v-model="drawer" app class="hidden-md-and-up nav">
+            <v-list>
+                <v-list-tile>
+                <v-list-tile-content>
+                    <v-layout column>
+                    <h3 class="text-center mb-4"></h3>
+                    <v-btn class="text-center mx-4" text>Home</v-btn>
+                    <v-btn  class="text-center mx-4" text>About Us</v-btn>
+                    <v-btn  class="text-center mx-4" text>Shop</v-btn>
+                    <v-btn class="text-center mx-4" text>Contact</v-btn>
+                    </v-layout>
+                </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+            </v-navigation-drawer>
+        </div>
+        <div class="progress mr-6 hidden-sm-and-down">
+            <v-btn icon><i class="bx bx-checkbox"></i></v-btn>
+            <v-btn icon><i class="bx bx-checkbox"></i></v-btn>
+            <v-btn icon><i class="bx bx-checkbox"></i></v-btn>
+            <v-btn icon><i class="bx bxs-checkbox"></i></v-btn>
+        </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            drawer: false
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+.toolbar {
+  width: 100%;
+  position: absolute;
+  top: 0;
+  background: rgba(255, 255, 255, 0.5);
+}
+
+.progress {
+  position: absolute;
+  right: 2px;
+  top: 30%;
+}
+
+.navigationLinks ul {
+  list-style: none;
+}
+
+.navigationLinks ul li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+.navigationLinks ul li a {
+  text-decoration: none;
+  color: black;
+}
+
+.logo {
+  position: relative;
+  top: 1.5rem;
+  cursor: pointer;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,13 @@
 <template>
   <div class="home">
-    <div class="imageDiv">
-      <img height="500" width="100%" :src="require('@/assets/main_image.png')">
-      <div class="toolbar d-flex justify-space-between align-center px-4">
-        <img class="logo" width="80" :src="require('@/assets/logo.png')">
-        <div class="navigationLinks">
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">Shop</a></li>
-            <li><a href="#">Contact</a></li>
-            <li>
-              <v-btn icon color="black"><i class="bx bx-search-alt-2 "></i></v-btn>
-            </li>
-            <li>
-              <v-btn icon color="black"><i class="bx bxs-lock-alt"></i></v-btn>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div class="progress mr-6">
-        <v-btn icon><i class="bx bx-checkbox"></i></v-btn>
-        <v-btn icon><i class="bx bx-checkbox"></i></v-btn>
-        <v-btn icon><i class="bx bx-checkbox"></i></v-btn>
-        <v-btn icon><i class="bx bxs-checkbox"></i></v-btn>
-      </div>
+    <div>
+      <navigation-bar />
     </div>
 
-    <div class="products my-12">
+    <div class="hidden-sm-and-down products my-12">
       <h3 class="text-center mb-4">POPULAR PRODUCTS</h3>
       <div class="cards d-flex justify-center">
-        <div>
+        <div class="">
           <v-card
           class="card mx-2"
           width="250"
@@ -49,7 +26,27 @@
       </div>
     </div>
 
-    <div class="gallery mt-12 mb-6 pa-5">
+    <div class="hidden-md-and-up products my-12">
+      <h3 class="text-center mb-4">POPULAR PRODUCTS</h3>
+      <div class="cards d-flex justify-center">
+        <div class="pa-4">
+          <v-card
+          class="card my-2"
+          v-for="(prod, i) in products"
+          :key="i">
+            <img width="250" :src="prod.img">
+            <v-card-title>{{ prod.title }}</v-card-title>
+            <v-card-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem?</v-card-text>
+            <v-card-actions>
+              <v-btn class="btns" text color="red">buy now</v-btn>
+              <v-card-text style="position: relative; left: 60%">$ {{ prod.price }}</v-card-text>
+            </v-card-actions>
+          </v-card>
+        </div>
+      </div>
+    </div>
+
+    <div class="hidden-sm-and-down gallery mt-12 mb-6 pa-5">
       <div class="pa-1" style="width: 60%">
         <img width="100%" height="100%" :src="require('@/assets/image_5.png')">
       </div>
@@ -61,11 +58,24 @@
         <img height="48%" width="100%" :src="require('@/assets/image_4.png')">
       </div>
     </div>
+
+    <div class="hidden-md-and-up pa-4">
+      <div class="why d-flex justify-center align-center mb-2">
+        <div>WHY US</div>
+      </div>
+      <img width="100%" height="240" :src="require('@/assets/image_5.png')">
+      <img width="100%" height="240" :src="require('@/assets/image_4.png')">
+    </div>
   </div>
 </template>
 
 <script>
+import navigation from '@/components/navigation.vue'
+
 export default {
+  components: {
+    'navigation-bar': navigation
+  },
   data() {
     return {
       products: [
@@ -95,38 +105,6 @@ export default {
 
 <style scoped>
 
-.toolbar {
-  width: 100%;
-  position: absolute;
-  top: 0;
-  background: rgba(255, 255, 255, 0.5);
-}
-
-.progress {
-  position: absolute;
-  right: 2px;
-  top: 30%;
-}
-
-.navigationLinks ul {
-  list-style: none;
-}
-
-.navigationLinks ul li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-.navigationLinks ul li a {
-  text-decoration: none;
-  color: black;
-}
-
-.logo {
-  position: relative;
-  top: 1.5rem;
-}
-
 .card {
   display: inline-block;
 }
@@ -145,5 +123,7 @@ export default {
   background-image: url(../assets/image_6.png);
   background-size: cover;
   font-size: 25px;
+  color: white;
+  height: 200px;
 }
 </style>
